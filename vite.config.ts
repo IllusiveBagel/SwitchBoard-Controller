f236145345139b24f3b38dea64c5487374a4b740
logan.young <logan.young@autoprotectgroup.co.uk>
2023-09-26 23:09:04 +0100

    Altered Vite Config to notBundle

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,24 @@
 import { defineConfig } from 'vite'
-import path from 'node:path'
-import electron from 'vite-plugin-electron/simple'
+//import path from 'node:path'
+import electron from 'vite-plugin-electron'
 import react from '@vitejs/plugin-react'
+import { notBundle } from 'vite-plugin-electron/plugin'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     react(),
     electron({
-      main: {
-        // Shortcut of `build.lib.entry`.
-        entry: 'electron/main.ts',
+        entry: [
+          'electron/main.ts',
+          'electron/preload.ts'
+        ],
+        vite: {
+          plugins: [
+            command === 'serve' && notBundle(/* NotBundleOptions */),
+          ],
+        },
       },
-      preload: {
-        // Shortcut of `build.rollupOptions.input`.
-        // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
-        input: path.join(__dirname, 'electron/preload.ts'),
-      },
-      // Ployfill the Electron and Node.js built-in modules for Renderer process.
-      // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
-      renderer: {},
-    }),
+    ),
   ],
-})
+}));
