d22912b8298316c326098f5ec06f6e7db97676ca
logan.young <logan.young@autoprotectgroup.co.uk>
2023-09-26 21:22:47 +0100

    Removed Sandbox

--- electron/main.ts
+++ electron/main.ts
@@ -26,6 +26,7 @@ function createWindow() {
     fullscreen: true,
     icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
     webPreferences: {
+      sandbox: false,
       preload: path.join(__dirname, 'preload.js'),
     },
   })
