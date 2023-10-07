c949be550c3a13eaaaf407edfc5b5bf4e38a93e3
logan.young <logan.young@autoprotectgroup.co.uk>
2023-10-07 03:37:31 +0100

    Testing using plugins

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 //import path from 'node:path'
 import electron from 'vite-plugin-electron'
+import renderer from 'vite-plugin-electron-renderer'
 import react from '@vitejs/plugin-react'
 import { notBundle } from 'vite-plugin-electron/plugin'
 
@@ -21,5 +22,6 @@ export default defineConfig(({ command }) => ({
 			},
 		},
 		),
+		renderer(),
 	],
 }));
